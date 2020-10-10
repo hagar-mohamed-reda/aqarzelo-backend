ba488507606ea91983a6b1e0695d0b3486251c13
hagar-mohamed-reda <gogo202046@yahoo.com>
2020-10-10 11:32:10 +0200

    create post image validation

--- public/website/js/createPost.js
+++ public/website/js/createPost.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -42,14 +42,14 @@ function uploadMasterImage(img, event, src, file) {
         file: file,
         is_360: false
     };
-     
-    app.post.images.push({ src: ''});
-    app.post.images.pop(); 
 
-    $('#masterDrop').css("background-image", "url('" + src + "')"); 
+    app.post.images.push({ src: ''});
+    app.post.images.pop();
+
+    $('#masterDrop').css("background-image", "url('" + src + "')");
     console.log($('#masterDrop')[0].style);
-    
-    
+
+
     if (img)
         img.src = src;
     $("#masterImage").attr('src', src);
@@ -61,40 +61,44 @@ function gotoStep(s) {
         if (app.post.images[0] == undefined)
             return error(masterImageError);
     }
-    
+
     if (s == 3) {
         if (!apiToken) {
             return error(login_first);
         }
     }
-    
+
+    if (s == 5) {
+        if (app.post.images.length < 2)
+            return error(image_error);
+    }
 
     step = s;
 
     $(".step").hide();
     $(".step-" + s).show();
-    
+
     if (s == 1 || s == 2) {
         s = 1;
     }
-    
+
     if (s == 3 || s == 4) {
         //$()fill_required_data_msg
         s = 2;
     }
-     
+
     if (s == 5) {
         s = 3;
     }
-    
-    // fill the circle 
+
+    // fill the circle
     $(".step-post-button").css("background-color", "white");
     $(".step-btn-" + s).css("background-color", "#06d9b2");
 }
- 
- 
+
+
 function gotoStep2(s) {
-    
+
     if (s == 2) {
         if (
             !app.post.title ||
@@ -106,33 +110,33 @@ function gotoStep2(s) {
         ) {
             return error(fill_required_data_msg);
         }
-        
+
         if (app.post.category_id != 4) {
             if (
-                !app.post.bedroom_number || 
-                !app.post.bathroom_number  
+                !app.post.bedroom_number ||
+                !app.post.bathroom_number
             ) {
                 return error(fill_required_data_msg);
-            }    
+            }
         }
         if (app.post.type != 'rent') {
             if (
-                !app.post.price_per_meter   
+                !app.post.price_per_meter
             ) {
                 return error(fill_required_data_msg);
-            }    
+            }
         }
     }
-    
+
     $(".post-data-step").hide();
     $(".post-data-step-" + s).show();
-    
-    // fill the circle 
+
+    // fill the circle
     $(".post-data-step-circle").removeClass("active");
     $(".post-data-step-circle-" + s).addClass("active");
 }
 
-function changeArea(city) { 
+function changeArea(city) {
     $('.area-select option').hide();
     $('.area-select option[city='+city+']').show();
 }
@@ -153,8 +157,8 @@ function isValid() {
        error(choose_location);
        valid = false;
     }
-    if (   
-            !app.post.owner_type || 
+    if (
+            !app.post.owner_type ||
             !app.post.city_id ||
             !app.post.area_id ||
             !app.post.payment_method ||
@@ -162,15 +166,15 @@ function isValid() {
        error(fill_all_data);
        valid = false;
     }
-    
-    
+
+
     return valid;
 }
 
 function getPostParams() {
     var fitlerImages = app.post.images.filter(function(value, index, arr){
         return value;
-    }); 
+    });
     var post = app.post;
     post.images = null;
     var string = $.param(post);
@@ -182,23 +186,23 @@ function getPostParams() {
 function savePost() {
     if (!apiToken)
         return error(login_first);
-     
+
     if (!isValid())
         return isValid();
-        
-    
+
+
 
     images = app.post.images.filter(function(value, index, arr){
         return value;
-    }); 
+    });
 
-    // show loading 
+    // show loading
     $(".post-loader").show();
-     
+
     var data = "";
-     
+
     data += "api_token=" + apiToken + "&" + getPostParams();
-      
+
     // reverse array
     images.reverse();
 
@@ -211,26 +215,26 @@ function savePost() {
                 success(response.message_en);
             if (LANG == "ar")
                 success(response.message_ar);
-             
-            // 
+
+            //
             for(var i = 0; i < app.post.images.length; i ++) {
-                images[i].post_id =  response.data.id; 
-                app.post.images[i].post_id =  response.data.id; 
+                images[i].post_id =  response.data.id;
+                app.post.images[i].post_id =  response.data.id;
             }
 
             savePostImages(images);
-    
+
         } else {
             if (LANG == "en")
                 error(response.message_en);
             if (LANG == "ar")
                 error(response.message_ar);
         }
-        
+
     });
 }
 
-function savePostImages(imgs) { 
+function savePostImages(imgs) {
     if (imgs.length <= 0)
         return finish();
 
@@ -243,14 +247,14 @@ function savePostImages(imgs) {
     formData.append("is_360", image.is_360);
     formData.append("api_token", apiToken);
     formData.append("post_id", image.post_id);
-    
+
     $.ajax({
         url: public_path + "/api/post/add-image",
         type: 'POST',
         data: formData,
         processData: false, // tell jQuery not to process the data
         contentType: false, // tell jQuery not to set contentType
-        success: function (data) {  
+        success: function (data) {
 //            if (response.status == 1) {
 //                if (LANG == "en")
 //                    success(response.message_en)
@@ -261,7 +265,7 @@ function savePostImages(imgs) {
 //                    error(response.message_en)
 //                if (LANG == "ar")
 //                    error(response.message_ar)
-//            } 
+//            }
             app.uploadedImageCount += 1;
             savePostImages(imgs)
         }
@@ -271,7 +275,7 @@ function savePostImages(imgs) {
 function finish() {
     $(".post-loader").hide();
     window.location = public_path + "/profile?state=pending";
-    
+
 }
 
 
@@ -284,7 +288,7 @@ var sort = null;
 function resortImages() {
     var images = app.post.images.filter(function(value, index, arr){ return value });
     var sortedImages = [];
- 
+
     var index = 0;
     $("#sortable li").each(function(){
         var liIndex = $(this).attr("data-index");
@@ -306,6 +310,6 @@ function resortImages() {
     $( "#sortable" ).disableSelection();
   } );
 
- 
+
 
   console.log(sort);
