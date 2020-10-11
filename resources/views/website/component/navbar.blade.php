86e24478ce5e4ea553bf7c86601f2f13e1a5da29
hagar-mohamed-reda <gogo202046@yahoo.com>
2020-10-11 13:49:51 +0200

    flag of english

--- resources/views/website/component/navbar.blade.php
+++ resources/views/website/component/navbar.blade.php
@@ -122,7 +122,7 @@ else
                     <li class="w3-hover-light-gray w3-padding" style="height: 30px!important" >
                         <a href="{{ url('/locale/en') }}" class="w3-display-container">
 
-                            <img src="{{ url('/images/english.svg') }}" class="fa w3-round w3-left" width="30px" >
+                            <img src="{{ url('/images/english.jpg') }}" class="fa w3-round w3-left" width="30px" >
 
                             <span class="w3-right" >English</span>
                         </a>
